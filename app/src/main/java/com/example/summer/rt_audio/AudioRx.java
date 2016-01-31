05a909ffb69e6c7d5646f26ddd679516201c7aa8
jacquestkirk <jacquestkirk@gmail.com>
2016-01-31 14:12:17 -0600

    refactor to seperate AudioRx stuff from GUI stuff, use handler with a different period to update the textboxes

---
+++ app/src/main/java/com/example/summer/rt_audio/AudioRx.java
@@ -0,0 +1,123 @@
+package com.example.summer.rt_audio;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.widget.TextView;
+
+import com.example.summer.rt_audio.R;
+
+/**
+ * Created by Summer on 1/31/2016.
+ */
+public class AudioRx {
+
+
+    private int DEFAULT_BUFFERSIZE_WORDS = 2048;
+    private int DEFAULT_WORDSIZE_BYTES = 2;
+    private int DEFAULT_SAMPRATE_HZ = 8000;
+    private int DEFAULT_CHANNELCONFIG = AudioFormat.CHANNEL_IN_MONO;
+    private int DEFAULT_AUDIOENCODEING = AudioFormat.ENCODING_PCM_16BIT;
+
+    private int wordSize_bytes;
+    private int bufferSize_words;
+    private int sampleRate_Hz;
+    private int channelConfig;
+    private int audioEncoding;//16 bit encoding because of 2 byte words
+    private int bufferSize_bytes;
+
+
+    private AudioRecord recorder;
+
+    private float sound_level;
+    private boolean isRecording = false;
+
+
+    public AudioRx()
+    {
+
+        wordSize_bytes = DEFAULT_WORDSIZE_BYTES; //Todo: Make this a funcition of audioEncoding instead of making this hard coded.
+        bufferSize_words = DEFAULT_BUFFERSIZE_WORDS;
+
+        sampleRate_Hz =DEFAULT_SAMPRATE_HZ;
+        channelConfig = DEFAULT_CHANNELCONFIG;
+        audioEncoding = DEFAULT_AUDIOENCODEING;
+        bufferSize_bytes = wordSize_bytes * bufferSize_words;
+
+        //Configure AudioRecord Object
+        recorder = new AudioRecord(MediaRecorder.AudioSource.MIC , sampleRate_Hz, channelConfig, audioEncoding, bufferSize_bytes);
+        recorder.setRecordPositionUpdateListener(updateListener);
+        recorder.setPositionNotificationPeriod(bufferSize_words); //Todo: Figure out what the units of frames is? That is the unit for the input to this function
+
+
+        int recorderState = recorder.getState();
+        if(recorderState==AudioRecord.STATE_UNINITIALIZED)
+        {
+            //Todo: Add an exception here showing that we were not able to initialize properly.
+        }
+    }
+
+    private AudioRecord.OnRecordPositionUpdateListener updateListener = new AudioRecord.OnRecordPositionUpdateListener(){
+
+        @Override
+        public void onPeriodicNotification(AudioRecord recorder){
+            processData();
+        }
+
+        @Override
+        public void onMarkerReached(AudioRecord recorder)
+        {}
+    };
+
+    private void processData()
+    {
+        short[] buffDat = new short[bufferSize_words];
+
+        //while (isRecording)
+        //{
+        recorder.read(buffDat, 0, bufferSize_words);
+
+        float sound_level_calc= 0;
+
+        for ( int i = 0; i <bufferSize_words; i++)
+        {
+            sound_level_calc += (Math.pow(buffDat[i],2)); //add up squares
+        }
+
+        sound_level = sound_level_calc; //push the newly calculated sound level to the class variable
+
+    }
+
+    public float getSoundLevel()
+    {
+        return sound_level;
+    }
+
+    public void getValidSampleRates() {
+        for (int rate : new int[] {8000, 11025, 16000, 22050, 44100}) {  // add the rates you wish to check against
+            int bufferSize = AudioRecord.getMinBufferSize(rate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+            if (bufferSize > 0) {
+                // buffer size is valid, Sample rate supported
+
+            }
+        }
+    }
+
+    public void startRx()
+    {
+        isRecording = true;
+        recorder.startRecording();
+    }
+
+    public void stopRx()
+    {
+        recorder.stop();
+        isRecording = false;
+    }
+
+    public boolean getRecordingState()
+    {
+        return isRecording;
+    }
+
+}
--- app/src/main/java/com/example/summer/rt_audio/MainActivity.java
+++ app/src/main/java/com/example/summer/rt_audio/MainActivity.java
@@ -10,167 +10,88 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import android.os.Handler;
+import java.util.logging.LogRecord;
 
 // thanks to http://stackoverflow.com/questions/8499042/android-audiorecord-example
 
 public class MainActivity extends AppCompatActivity {
-    private AudioRecord recorder;
-    private boolean isRecording = false;
-    private Thread recordingThread;
-
-    private int BufferSize_words = 2048;
-    private int WordSize_bytes = 2;
+    private AudioRx receiver;
 
     private EditText sumSqEditText;
     private EditText logEditText;
 
-    private float sound_level;
+    private static long updateTextBoxInterval_ms = 100;
+    private Handler editTextUpdateHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int sampleRate_Hz =8000;
-        int channelConfig = AudioFormat.CHANNEL_IN_MONO;
-        int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;//16 bit encoding because of 2 byte words
-        int bufferSize_bytes = WordSize_bytes * BufferSize_words;
-
-
-        recorder = new AudioRecord(MediaRecorder.AudioSource.MIC , sampleRate_Hz, channelConfig, audioEncoding, bufferSize_bytes);
-        recorder.setRecordPositionUpdateListener(updateListener);
-        recorder.setPositionNotificationPeriod(BufferSize_words);
-        int recorderState = recorder.getState();
-
-        if(recorderState==AudioRecord.STATE_UNINITIALIZED)
-        {
-            //AudioRecord did not construct properly
-            TextView myTextView = (TextView) findViewById(R.id.textView);
-            myTextView.setText("Could not start AudioRecord: Do not press the button or you will crash!!!");
-        }
 
         //Set up EditTexts
-
         sumSqEditText = (EditText) findViewById(R.id.sumSqEditText);
         logEditText = (EditText) findViewById(R.id.logEditText);
 
         sumSqEditText.setText("Hello");//String.format("%.2f", sound_level));
-        logEditText.setText("workd");
+        logEditText.setText("world");
 
-        getValidSampleRates();
+        //Set up handler to update text boxes
+        editTextUpdateHandler = new Handler();
+
+
+        //Set up Rx
+        receiver = new AudioRx();
+
 
     }
-
-    public void getValidSampleRates() {
-        for (int rate : new int[] {8000, 11025, 16000, 22050, 44100}) {  // add the rates you wish to check against
-            int bufferSize = AudioRecord.getMinBufferSize(rate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
-            if (bufferSize > 0) {
-                // buffer size is valid, Sample rate supported
-
-            }
-        }
-    }
-
-    private AudioRecord.OnRecordPositionUpdateListener updateListener = new AudioRecord.OnRecordPositionUpdateListener(){
-
-        @Override
-        public void onPeriodicNotification(AudioRecord recorder){
-            processData();
-        }
-
-        @Override
-        public void onMarkerReached(AudioRecord recorder)
-        {}
-    };
-
 
     private void sandbox()
     {
-        processData();
+        //processData();
     }
 
 
     public void goButtonClick(View view)
     {
-        if(isRecording)
+        if(receiver.getRecordingState())
         {
-            isRecording = false;
+            receiver.stopRx();
         }
         else{
-            isRecording = true;
-            startRec();
+            receiver.startRx();
+            editTextUpdateHandler.postDelayed(updateTextBoxes, updateTextBoxInterval_ms);
         }
 
     }
 
-    private void startRec()
-    {
-        recorder.startRecording();
 
-        //start a thread to record data
-        //try this later, right now we just want to print results.
-        /*recordingThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                processData();
-            }
-        });
+    //repetative task that updates text boxes
 
-        recordingThread.start();
-*/
-        //sandbox();
+    private Runnable updateTextBoxes = new Runnable() {
+        @Override
+        public void run() {
+            //Get sound level from receiver
+            float current_sound_level = receiver.getSoundLevel();
+            //update textboxes
+            sumSqEditText.setText(String.format("%f2", current_sound_level));
+            logEditText.setText(String.format("%f2", Math.log10(current_sound_level)));
 
-        /*while(true)
-        {
-
-            try{
-                Thread.sleep(100,0);
-            }catch(InterruptedException e)
+            //if we're still recording, schedule this task again
+            if (receiver.getRecordingState())
             {
-                //I don't really give a shit if this is interrupted;
+                editTextUpdateHandler.postDelayed(updateTextBoxes,updateTextBoxInterval_ms);
             }
 
-
-            sumSqEditText.setText(String.format("%.2f", sound_level));
-            sumSqEditText.setText(String.format("%.2f", Math.log10( sound_level)));
 
 
         }
-        */
-    }
+    };
 
 
 
-    private void processData()
-    {
-        short[] buffDat = new short[BufferSize_words];
 
-        //while (isRecording)
-        //{
-            recorder.read(buffDat, 0, BufferSize_words);
-
-            float sound_level_calc= 0;
-
-            for ( int i = 0; i <BufferSize_words; i++)
-            {
-                sound_level_calc += (Math.pow(buffDat[i],2)); //add up squares
-            }
-
-           sound_level = sound_level_calc; //push the newly calculated sound level to the class variable
-
-            sumSqEditText.setText(String.format("%.2f", sound_level));
-            logEditText.setText(String.format("%.2f", Math.log10( sound_level)));
-
-            /*try{
-                Thread.sleep(1000,0);
-            }catch(InterruptedException e)
-            {
-                //I don't really give a shit if this is interrupted;
-            }*/
-        //}
-
-
-    }
 
 }
 
